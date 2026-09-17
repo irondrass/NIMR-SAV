0ad88c62ebf2c246430b95009cf91b5dd9ff3b13
Khaled Mhadhbi <khaled.mhadhbi@nimr.com.tn>
2026-09-17 22:37:51 +0100

    chore(release): publish Reception R1 v23.3.52

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-window.APP_VERSION = "v23.3.51";
-window.NIMR_BUILD = "v23.3.51";
-window.NIMR_CACHE_NAME = "nimr-sav-v23.3.51";
+window.APP_VERSION = "v23.3.52";
+window.NIMR_BUILD = "v23.3.52";
+window.NIMR_CACHE_NAME = "nimr-sav-v23.3.52";
